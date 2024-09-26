455e344dbac8e3ec2e62918c37153b0f3f1d0006
JeremiahLare09 <jeremiahl2027@student.usd105.org>
2024-09-26 10:35:11 -0500

    Update map and code

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -6,9 +6,11 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
+	print("death")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start(.06)
-	print("died")
 	
-
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
--- slime.gd
+++ slime.gd
@@ -6,14 +6,17 @@ extends Node2D
 const SPEED = 60
 var direction = 1
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var ray_right: RayCast2D = $"Ray right"
 @onready var ray_left: RayCast2D = $"Ray left"
 
 func _process(delta: float) -> void:
 	if ray_right.is_colliding():
 		direction = -1
+		animated_sprite_2d.flip_h = true
 	if ray_left.is_colliding():
 		direction = 1
+		animated_sprite_2d.flip_h = false
 	
 	position.x += direction * SPEED * delta
 	
