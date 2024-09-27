6ee8f6782270d8e3c0b8a7d7158ec20387a65ca7
JeremiahLare09 <jeremiahl2027@student.usd105.org>
2024-09-27 10:31:02 -0500

    UPT bug fixes enemy

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -3,13 +3,14 @@ extends Area2D
 @onready var timer = $Timer
 
 
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(body) -> void:
 	print("death")
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
-	timer.start(.06)
+	timer.start(.5)
 	
 func _on_timer_timeout() -> void:
 	Engine.time_scale = 1
