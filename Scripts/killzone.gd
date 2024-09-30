78c9b1852539f59639c82a2ee754410a6e121654
Buffylare09 <jeremiahl2027@student.usd105.org>
2024-09-29 22:00:16 -0500

    sm upt

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var timer = $Timer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
