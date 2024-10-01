bf21171a9ff98ef9d7c732b06dd680d2aa3a0dad
JeremiahLare09 <jeremiahl2027@student.usd105.org>
2024-10-01 10:39:28 -0500

    Coin upt

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,9 +1,10 @@
 extends Node
 
+@onready var score_label: Label = $"Score label"
 
 var score = 0
 
 
 func add_point():
 	score += 1
-	print(score)
+	score_label.text = "you collected " + str(score) + " coins."
